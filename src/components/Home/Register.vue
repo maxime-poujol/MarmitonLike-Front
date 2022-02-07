8abfdd05ba0b0532aea61f41e1619e8e45a2b715
maxime <max.poujol21@gmail.com>
2022-02-07 10:37:32 +0100

    register

--- src/components/Home/Register.vue
+++ src/components/Home/Register.vue
@@ -1,16 +1,46 @@
 <template>
-  <LoginContainer>
-  </LoginContainer>
+  <div>
+    <form>
+      <!-- Ici je relie mes input aux données de mon composant avec un v-model -->
+      <div>
+        <input id="userMail" v-model="username" type="email" name="email" required>
+        <label for="userMail">E-mail</label>
+      </div>
+      <div>
+        <input id="username" v-model="username" type="text" name="username">
+        <label for="username"></label>
+
+        <input id="lastname" v-model="lastname" class="form-control" placeholder="Nom" required type="text"/>
+        <label for="lastname">Nom</label>
+      </div>
+      <div class="form-floating my-2">
+        <input id="subject" v-model="subject" class="form-control" placeholder="Sujet de la demande" required
+               type="text"/>
+        <label for="subject">Sujet de la demande </label>
+      </div>
+      <div class="form-floating my-2">
+        <textarea id="message" v-model="content" class="form-control" placeholder="Message" required></textarea>
+        <label for="message">Message</label>
+      </div>
+      <div class="text-center">
+        <button class="btn btn-primary" type="submit" @click="submitForm">Soumettre</button>
+      </div>
+    </form>
+
+
+
+
+
+    <input id="password" type="password" name="password">
+    <label for="password"></label>
+
+
+
+  </div>
 </template>
 
 <script>
-import {LoginContainer} from "@/styles/Home/Form.style";
 export default {
-  name: "Register",
-  components: {LoginContainer}
+  name: "Register"
 }
 </script>
-
-<style scoped>
-
-</style>
