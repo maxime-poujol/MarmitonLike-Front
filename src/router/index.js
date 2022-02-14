f949ab03b974774b7abd65921579ac449fe3328a
maxime <max.poujol21@gmail.com>
2022-02-14 09:15:44 +0100

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router'
 import Homepage from "@/pages/Homepage";
 import Recepies from "@/pages/recipe/Recepies";
 import Recipe from "@/pages/recipe/Recipe";
+import MyRecipe from "@/pages/recipe/MyRecipe";
+import CreateRecipe from "@/pages/recipe/CreateRecipe";
+import UpdateRecipe from "@/pages/recipe/UpdateRecipe";
 
 Vue.use(VueRouter)
 
@@ -22,7 +25,27 @@ export default new VueRouter({
             component: Recipe,
             name: 'recipe',
             path: '/recipe/:id',
-        }
+        },
+        {
+            component: MyRecipe,
+            name: 'myRecipe',
+            path: '/myrecipe',
+        },
+        {
+            component: CreateRecipe,
+            name: 'myRecipe-create',
+            path: '/myrecipe/create',
+        },
+        {
+            component: UpdateRecipe,
+            name: 'myRecipe-update',
+            path: '/myrecipe/update',
+        },
+        {
+            component: UpdateRecipe,
+            name: 'myRecipe-delete',
+            path: '/myrecipe/delete',
+        },
 
     ]
 })
