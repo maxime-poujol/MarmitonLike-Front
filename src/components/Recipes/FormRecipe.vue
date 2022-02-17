8855d25bdd53de918500ba601a8d67542b87ebbe
maxime <max.poujol21@gmail.com>
2022-02-17 13:50:39 +0100

    Changes by naber : recipe

--- src/components/Recipes/FormRecipe.vue
+++ src/components/Recipes/FormRecipe.vue
@@ -2,21 +2,52 @@
   <FormContainer>
     <Form>
       <FieldContainer>
-        <Field id="name" v-model="rname" autofocus name="rname" tabindex="1" type="text"/>
-        <Field id="preparationTime" v-model="preparationTime" name="preparationTime" tabindex="2" type="text"/>
-        <Field id="cookingTime" v-model="cookingTime" name="cookingTime" tabindex="3" type="text"/>
-        <Field id="restTime" v-model="restTime" name="restTime" tabindex="4" type="text"/>
-        <Field id="difficulty" v-model="difficulty" name="difficulty" tabindex="5" type="text"/>
-        <Field id="image" v-model="image" name="image" tabindex="6" type="text"/>
-        <button type="submit">button</button>
+        <FormRow>
+          <label for="name">Name</label>
+          <Field id="name" v-model="rname" autofocus name="rname" tabindex="1" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label for="preparationTime">Preparation</label>
+          <Field id="preparationTime" v-model="preparationTime" name="preparationTime" tabindex="2" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label for="cookingTime">Cooking</label>
+          <Field id="cookingTime" v-model="cookingTime" name="cookingTime" tabindex="3" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label for="restTime">Rest</label>
+          <Field id="restTime" v-model="restTime" name="restTime" tabindex="4" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label for="difficulty">Difficulty</label>
+          <Field id="difficulty" v-model="difficulty" name="difficulty" tabindex="5" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label for="image">Image</label>
+          <Field id="image" v-model="image" name="image" tabindex="6" type="text"/>
+        </FormRow>
+        <FormRow>
+          <label>Ingredients</label>
+          <FormGroup>
+            <IngredientRow id="ingredients-row"></IngredientRow>
+            <div>
+              <button type="button" @click="createIngredientRow()">PLUS</button>
+            </div>
+          </FormGroup>
+
+
+        </FormRow>
+
+        <Submit type="submit" value="button"/>
+
       </FieldContainer>
     </Form>
   </FormContainer>
 </template>
 
 <script>
-import {Field, FieldContainer, Form} from "@/styles/Home/Form.style";
-import {FormContainer} from '@/styles/recipes/FormRecipe.style'
+import {Field, FieldContainer, Form, Submit} from "@/styles/Home/Form.style";
+import {FormContainer, FormRow, IngredientRow, FormGroup} from '@/styles/recipes/FormRecipe.style'
 
 export default {
   name: "FormRecipe",
@@ -27,14 +58,34 @@ export default {
       cookingTime: "",
       restTime: "",
       difficulty: "",
-      image: ""
+      image: "",
     }
   },
   components: {
     Form,
     FieldContainer,
     Field,
-    FormContainer
+    FormContainer,
+    FormRow,
+    Submit,
+    IngredientRow,
+    FormGroup
+  },
+  mounted() {
+    this.createIngredientRow()
+  },
+  methods: {
+    createIngredientRow() {
+      const div = document.getElementById("ingredients-row");
+
+      const row = document.createElement("div");
+      const nameInput = document.createElement("input");
+      const qteInput = document.createElement("input");
+      row.appendChild(nameInput);
+      row.appendChild(qteInput);
+
+      div.appendChild(row);
+    }
   }
 }
 </script>
