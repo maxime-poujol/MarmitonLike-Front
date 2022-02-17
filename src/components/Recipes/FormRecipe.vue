d3f44ca930cc92a3244bafda5ae1d897e8831c59
maxime <max.poujol21@gmail.com>
2022-02-17 11:51:18 +0100

    recipe

--- src/components/Recipes/FormRecipe.vue
+++ src/components/Recipes/FormRecipe.vue
@@ -1,12 +1,17 @@
 <template>
-  <Form>
-    <input type="text" name="name" id="name">
-    <input type="text" name="preparationTime" id="preparationTime">
-    <input type="text" name="cookingTime" id="cookingTime">
-    <input type="text" name="restTime" id="restTime">
-    <input type="text" name="difficulty" id="difficulty">
-    <input type="text" name="image" id="image">
-  </Form>
+  <FormContainer>
+    <Form>
+      <FieldContainer>
+        <Field type="text" v-model="rname" name="rname" id="name" tabindex="1" autofocus/>
+        <Field type="text" v-model="preparationTime" name="preparationTime" id="preparationTime" tabindex="2"/>
+        <Field type="text" v-model="cookingTime" name="cookingTime" id="cookingTime" tabindex="3"/>
+        <Field type="text" v-model="restTime" name="restTime" id="restTime" tabindex="4"/>
+        <Field type="text" v-model="difficulty" name="difficulty" id="difficulty" tabindex="5"/>
+        <Field type="text" v-model="image" name="image" id="image" tabindex="6"/>
+        <button type="submit">button</button>
+      </FieldContainer>
+    </Form>
+  </FormContainer>
 </template>
 
 <script>
@@ -14,6 +19,16 @@ import {Form} from "@/styles/Home/Form.style";
 
 export default {
   name: "FormRecipe",
+  data() {
+    return {
+      rname: "",
+      preparationTime: "",
+      cookingTime: "",
+      restTime: "",
+      difficulty: "",
+      image: ""
+    }
+  },
   components: {
     Form
   }
---
+++ src/styles/recipes/FormRecipe.style.js
@@ -0,0 +1,6 @@
+import styled from 'vue-styled-components';
+
+export const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
