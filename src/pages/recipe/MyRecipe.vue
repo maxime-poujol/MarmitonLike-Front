3aef493bebae1d605d95fb66bb39722a3e32c149
maxime <max.poujol21@gmail.com>
2022-02-17 11:39:20 +0100

    recipe

--- src/pages/recipe/MyRecipe.vue
+++ src/pages/recipe/MyRecipe.vue
@@ -1,38 +1,44 @@
 <template>
-  <section>
-    <table>
-      <thead>
-      <tr>
-        <th>ID</th>
-        <th>name</th>
-        <th>preparationTime</th>
-        <th>cookingTime</th>
-        <th>restTime</th>
-        <th>totalTime</th>
-        <th>Image</th>
-        <th></th>
-        <th></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(recipe,key) in userRecipes" :key="key">
-        <td>{{recipe.id}}</td>
-        <td>{{recipe.name}}</td>
-        <td>{{recipe.preparationTime}}</td>
-        <td>{{recipe.cookingTime}}</td>
-        <td>{{recipe.restTime}}</td>
-        <td>{{recipe.totalTime}}</td>
-        <td>{{recipe.image}}</td>
-        <td>
-          <router-link :to="'/myrecipes/update/' + recipe.id">Modifier</router-link>
-        </td>
-        <td>
-          <router-link :to="'/myrecipes/delete' + recipe.id">Supprimer</router-link>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-  </section>
+  <div>
+    <section>
+      <router-link :to="{name: 'myRecipe-create'}"> Create a recipe </router-link>
+    </section>
+    <section>
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>name</th>
+          <th>preparationTime</th>
+          <th>cookingTime</th>
+          <th>restTime</th>
+          <th>totalTime</th>
+          <th>Image</th>
+          <th></th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(recipe,key) in userRecipes" :key="key">
+          <td>{{recipe.id}}</td>
+          <td>{{recipe.name}}</td>
+          <td>{{recipe.preparationTime}}</td>
+          <td>{{recipe.cookingTime}}</td>
+          <td>{{recipe.restTime}}</td>
+          <td>{{recipe.totalTime}}</td>
+          <td>{{recipe.image}}</td>
+          <td>
+            <router-link :to="{name: 'myRecipe-update', params: {id: recipe.id}}">Modifier</router-link>
+          </td>
+          <td>
+            <button @click="onDelete(recipe)">Supprimer</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </section>
+  </div>
+
 
 </template>
 
